8bfe1fcf1f949b692d37e367a5982fa68e08382c
root <root@Alejandro.localdomain>
2023-05-15 18:34:25 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef} from 'react'
 import Todolist from './Todolist'
 
 export default function App() {
   const[todos, setTodos]= useState([])
   const todoNameRef = useRef()
+  const [count, setCount] = useState(0);
 
   function toggleTodo(id) {
     const newTodos = [...todos]
@@ -13,11 +14,14 @@ export default function App() {
   }
 
 function handleAddTodo(e) {
+  
   const name= todoNameRef.current.value
   if (name === '') return
   setTodos(previousTodos => {
-    return[...previousTodos, {id: 1, name: name, complete:false}]
+    setCount((count) => count + 1);
+    return[...previousTodos, {id:{count}, name: name, complete:false}]
   })
+
   todoNameRef.current.value = null
 }
 
@@ -31,7 +35,7 @@ function handleClearTodos(){
     <h1>My Todolist</h1>
     <input ref={todoNameRef} type ="text"/>
     <button onClick={handleAddTodo}>Add Todo</button>
-    <button onClick={handleClearTodos}>Clear</button>
+    <button onClick={handleClearTodos}>Clear Completed Todo</button>
     <Todolist todos={todos} toggleTodo = {toggleTodo}/>
     <div>{todos.filter(todo =>!todo.complete).length} left to do</div>
     </>
